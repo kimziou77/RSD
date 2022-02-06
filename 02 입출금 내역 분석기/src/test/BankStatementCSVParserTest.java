dbcfac73def41a9fa7c05f6df50818cbda4e1688
=kimziou77 <kimziou77@naver.com>
2022-02-07 03:22:53 +0900

    (test) sample test

---
+++ 02 입출금 내역 분석기/src/test/BankStatementCSVParserTest.java
@@ -0,0 +1,14 @@
+import main.BankStatementCSVParser;
+import main.BankStatementParser;
+import org.junit.jupiter.api.Test;
+
+class BankStatementCSVParserTest {
+    private final BankStatementParser statementParser = new BankStatementCSVParser();
+
+    @Test
+    public void shouldParseOneCorrectLine() throws Exception{
+        System.out.println("dkd");
+        // Assert.fail("Not yet implemented");
+    }
+
+}
