c137513fb7fb9e2417c552419eae13ffc35d78da
=kimziou77 <kimziou77@naver.com>
2022-02-07 00:23:54 +0900

    (refactor) Interface를 통한 결합도 낮추기

--- 02 입출금 내역 분석기/src/BankStatementAnalyzer.java
+++ 02 입출금 내역 분석기/src/BankStatementAnalyzer.java
@@ -8,20 +8,17 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BankTransactionAnalyzer {
+public class BankStatementAnalyzer {
     private static final String RESOURCES = "src/main/resources/";
-
-    public static void main(final String... args) throws IOException {
-        final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
-        final String fileName = args[0];
+    public void analyze(final String fileName, final BankStatementParser bankStatementParser) throws IOException{
         final Path path = Paths.get(RESOURCES + fileName);
         final List<String> lines = Files.readAllLines(path);
-        final List<BankTransaction> bankTransactions = bankStatementCSVParser.parseLinesFromCSV(lines);
+        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFrom(lines);
         final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
 
         collectSummary(bankStatementProcessor);
-
     }
+
     private static void collectSummary(final BankStatementProcessor bankStatementProcessor){
         System.out.println("The total for all transactions is " +
                 bankStatementProcessor.calculateTotalAmount());
--- 02 입출금 내역 분석기/src/BankStatementCSVParser.java
+++ 02 입출금 내역 분석기/src/BankStatementCSVParser.java
@@ -3,10 +3,12 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BankStatementCSVParser {
+public class BankStatementCSVParser implements BankStatementParser{
     private static final DateTimeFormatter DATE_PATTERN
             = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-    private BankTransaction parseFromCSV(final String line){
+
+    @Override
+    public BankTransaction parseFrom(final String line){
         final String[] columns = line.split(",");
 
         final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
@@ -15,10 +17,11 @@ public class BankStatementCSVParser {
 
         return new BankTransaction(date, amount, description);
     }
-    public List<BankTransaction> parseLinesFromCSV(final List<String> lines){
+    @Override
+    public List<BankTransaction> parseLinesFrom(final List<String> lines){
         final List<BankTransaction> bankTransactions = new ArrayList<>();
         for(final String line: lines){
-            bankTransactions.add(parseFromCSV(line));
+            bankTransactions.add(parseFrom(line));
         }
         return bankTransactions;
     }
