a705f133ae00ad61445f229faeecd65621449663
=kimziou77 <kimziou77@naver.com>
2022-02-07 10:16:34 +0900

    (test) BankStatementParser shouldParseOneCorrectLIne

--- 02 입출금 내역 분석기/src/test/java/BankStatementCSVParserTest.java
+++ 02 입출금 내역 분석기/src/test/java/BankStatementCSVParserTest.java
@@ -1,4 +1,8 @@
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankStatementCSVParserTest {
@@ -6,7 +10,15 @@ class BankStatementCSVParserTest {
 
     @Test
     public void shouldParseOneCorrectLine() throws Exception{
-        System.out.println("dkd");
-        fail("Not yet implemented");
+        final String line = "30-01-2017,-50,Tesco";
+        final BankTransaction result = statementParser.parseFrom(line);
+
+        final BankTransaction expected
+                = new BankTransaction(LocalDate.of(2017, Month.JANUARY,30),-50,"Tesco");
+        final double tolerance = 0.0d;
+
+        assertEquals(expected.getDate(), result.getDate());
+        assertEquals(expected.getAmount(), result.getAmount());
+        assertEquals(expected.getDescription(), result.getDescription());
     }
 }
